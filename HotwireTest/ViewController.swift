6dd141dcf0408a26cce2134cbba1448774d0e35a
Jonathan Jacob <jjjscf@gmail.com>
2024-11-25 08:52:06 +0100

    Initial Commit

---
+++ HotwireTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HotwireTest
+//
+//  Created by Jonathan Jacob on 25.11.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HotwireTestTests/HotwireTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  HotwireTestTests.swift
+//  HotwireTestTests
+//
+//  Created by Jonathan Jacob on 25.11.24.
+//
+
+import Testing
+@testable import HotwireTest
+
+struct HotwireTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
